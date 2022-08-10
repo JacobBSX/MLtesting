b51b145d8d01c94216101f84175de882c6c4a0ea
Jake Ellwood <JakeEllwood@JakeEllwood-MacBook-Pro.local>
2022-08-10 12:29:28 +0100

    Initialised project with main and data sorting library

---
+++ MLDataSorts.py
@@ -0,0 +1,16 @@
+# J Ellwood - 10/08/2022 - Sorting Algorithms for data
+
+#InsertionSort
+
+#BogoSort
+
+#MergeSort
+
+
+
+if __name__ == "__main__":
+    # Load test data
+    # TestData.Load yadayada
+    # Run Tests
+    print("Testing Complete")
+
---
+++ MLMain.py
@@ -0,0 +1,12 @@
+# J Ellwood - 10/08/2022 - Machine Learning Practice
+
+print("Hello World")
+
+if __name__ == "__main__":
+    print("This is the main")
+
+    # Load Practice Data
+
+    # Check/Transform Data
+
+    # 
